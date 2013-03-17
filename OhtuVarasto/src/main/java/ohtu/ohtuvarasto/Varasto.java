d29755d647b5893b46cfa713b133e8553cabc7c6
Teemu Hurme <teemu.hurme@cs.helsinki.fi>
2013-03-17 16:37:07 +0200

    Revert "testi rikki"
    This reverts commit 487e0d988a123789716653d184906bcd15a677f9.

--- OhtuVarasto/src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ OhtuVarasto/src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -67,7 +67,7 @@ public class Varasto {
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
-            double kaikkiMitaVoidaan = saldo+1;
+            double kaikkiMitaVoidaan = saldo;
             saldo = 0.0;               // ja tyhjäksi menee
             return kaikkiMitaVoidaan;  // poistutaan saman tien
         }
